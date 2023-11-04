c70b58337688ade67fcce08c79d1f718c835142c
Ashiq Cherian <ashiq.cherian@experionglobal.local>
2023-11-04 15:39:41 +0530

    feat:-Added rollDice functionality

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -1,2 +1,26 @@
-'use strict';
-//comment
+let current = 0;
+
+function rollDice() {
+    const random = Math.floor(Math.random() * 6) + 1;
+    if (random !== 1) {
+        current = current + random;
+    } else {
+        current = 0;
+        switchPlayer();
+    }
+    console.log("random = " + random);
+    console.log("current = " + current);
+    document.getElementById("current--0").textContent = current;
+    return current;
+}
+
+const change = switchPlayer();
+const currentVal = rollDice();
+
+console.log("Final current value = " + currentVal);
+
+
+
+// const changeBg=()=>{
+//     document.body.style.backgroundColor="red";
+// }
