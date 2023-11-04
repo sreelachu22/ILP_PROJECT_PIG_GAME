c8498f4bcbdb1ffe169c79b8e67d289ef059a17c
Avinash K R <avinash.kr@experionglobal.local>
2023-11-04 15:50:26 +0530

    fix

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -10,24 +10,23 @@ function reset() {
 let current = 0;
 
 function rollDice() {
-    const random = Math.floor(Math.random() * 6) + 1;
-    if (random !== 1) {
-        current = current + random;
-    } else {
-        current = 0;
-        switchPlayer();
-    }
-    console.log("random = " + random);
-    console.log("current = " + current);
-    document.getElementById("current--0").textContent = current;
-    return current;
+  const random = Math.floor(Math.random() * 6) + 1;
+  if (random !== 1) {
+    current = current + random;
+  } else {
+    current = 0;
+    switchPlayer();
+  }
+  console.log('random = ' + random);
+  console.log('current = ' + current);
+  document.getElementById('current--0').textContent = current;
+  return current;
 }
 
 const change = switchPlayer();
 const currentVal = rollDice();
 
-console.log("Final current value = " + currentVal);
-
+console.log('Final current value = ' + currentVal);
 
 //mehanoor
 let current1 = document.getElementsById('current--0');
@@ -35,19 +34,15 @@ let current2 = document.getElementsById('current--1');
 let container1 = document.getElementsByClassName('player player--0');
 let container2 = document.getElementsByClassName('player player--1');
 
-
 var content = document.createElement('h3');
-content.classList.add('playerwins' ,'');
+content.classList.add('playerwins', '');
 container1.appendChild(content);
-
 
 //nathaniel
 const switchPlayer = function () {
   const player1 = document.getElementsByClassName('player player--0')[0];
   const player2 = document.getElementsByClassName('player player--1')[0];
 
-
-  
   document.getElementById('current--0').textContent = 0;
   document.getElementById('current--1').textContent = 0;
 
@@ -56,14 +51,15 @@ const switchPlayer = function () {
 };
 
 //sreelakshmi
-function hold(){
-    let current0 = document.querySelector('current--0');
-    let current1 = document.querySelector('current--1');
-    let score0 = document.querySelector('score--0');
-    let score1 = document.querySelector('score--1');
-    document.getElementById('score--0').textContent = score0 + current0;
-    console.log(score0)
-    document.getElementById('score--1').textContent = score1 + current1;
-    switchPlayer()
+function hold() {
+  let current0 = document.querySelector('current--0');
+  let current1 = document.querySelector('current--1');
+  let score0 = document.querySelector('score--0');
+  let score1 = document.querySelector('score--1');
+  document.getElementById('score--0').textContent = score0 + current0;
+  console.log(score0);
+  document.getElementById('score--1').textContent = score1 + current1;
+  switchPlayer();
 }
 
+// nattu
