462a000b254422b41fef276cfe8548cf50c4e4c1
Nathaniel Yeldo <nathaniel.yeldo@experionglobal.local>
2023-11-04 15:25:01 +0530

    feat:-added the switch player feature

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -1,2 +1,10 @@
-'use strict';
-//comment
+const switchPlayer = function () {
+  const player1 = document.getElementsByClassName('player player--0')[0];
+  const player2 = document.getElementsByClassName('player player--1')[0];
+
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+};
