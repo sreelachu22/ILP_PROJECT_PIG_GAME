83d5ed7b03efb7b2beec2bcf1c8ea913df56b0ff
Mehanoor Basheer <mehanoor.basheer@experionglobal.local>
2023-11-04 17:07:24 +0530

    fix:merged all

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -1,4 +1,35 @@
+//avinash
+function reset() {
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+}
 
+//ashiq
+let current = 0;
+
+function rollDice() {
+    const random = Math.floor(Math.random() * 6) + 1;
+    if (random !== 1) {
+        current = current + random;
+    } else {
+        current = 0;
+        switchPlayer();
+    }
+    console.log("random = " + random);
+    console.log("current = " + current);
+    document.getElementById("current--0").textContent = current;
+    return current;
+}
+
+const change = switchPlayer();
+const currentVal = rollDice();
+
+console.log("Final current value = " + currentVal);
+
+
+//mehanoor
 let current1 = document.getElementsById('current--0');
 let current2 = document.getElementsById('current--1');
 let container1 = document.getElementsByClassName('player player--0');
@@ -8,4 +39,31 @@ let container2 = document.getElementsByClassName('player player--1');
 var content = document.createElement('h3');
 content.classList.add('playerwins' ,'');
 container1.appendChild(content);
+
+
+//nathaniel
+const switchPlayer = function () {
+  const player1 = document.getElementsByClassName('player player--0')[0];
+  const player2 = document.getElementsByClassName('player player--1')[0];
+
+
+  
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+};
+
+//sreelakshmi
+function hold(){
+    let current0 = document.querySelector('current--0');
+    let current1 = document.querySelector('current--1');
+    let score0 = document.querySelector('score--0');
+    let score1 = document.querySelector('score--1');
+    document.getElementById('score--0').textContent = score0 + current0;
+    console.log(score0)
+    document.getElementById('score--1').textContent = score1 + current1;
+    switchPlayer()
+}
 
