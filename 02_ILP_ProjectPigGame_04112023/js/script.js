bf0735666fe9cd80ed2475eac7ec18d327494aee
Avinash K R <avinash.kr@experionglobal.local>
2023-11-04 15:41:44 +0530

    feat commit

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -1,2 +1,6 @@
-'use strict';
-//comment
+function reset() {
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+}
