a013b4c2152c2ba9fe436170d84c4d5dc2ddd77e
Mehanoor Basheer <mehanoor.basheer@experionglobal.local>
2023-11-04 15:39:00 +0530

    feat:winning

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -1,2 +1,11 @@
-'use strict';
-//comment
+
+let current1 = document.getElementsById('current--0');
+let current2 = document.getElementsById('current--1');
+let container1 = document.getElementsByClassName('player player--0');
+let container2 = document.getElementsByClassName('player player--1');
+
+
+var content = document.createElement('h3');
+content.classList.add('playerwins' ,'');
+container1.appendChild(content);
+
