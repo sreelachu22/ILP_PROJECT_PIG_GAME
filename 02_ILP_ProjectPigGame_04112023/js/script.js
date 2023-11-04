988bd86f5464dc449ea47cf9740ea9a209d3a92b
Ashiq Cherian <ashiq.cherian@experionglobal.local>
2023-11-04 19:30:33 +0530

    feat:-Added total score, switch player functionalities

--- 02_ILP_ProjectPigGame_04112023/js/script.js
+++ 02_ILP_ProjectPigGame_04112023/js/script.js
@@ -3,16 +3,22 @@
 let current = 0;
 
 function rollDice() {
+    const playerNumber = getActivePlayer();
     const random = Math.floor(Math.random() * 6) + 1;
+    const diceImage = document.querySelector(".dice");
+    const imageName = `dice-${random}.png`;
+    diceImage.src = imageName;
     if (random !== 1) {
         current = current + random;
     } else {
         current = 0;
+        document.getElementById(`current--${playerNumber}`).textContent = current;
+        document.getElementById(`score--${playerNumber}`).textContent = 0;
         switchPlayer();
     }
     console.log("random = " + random);
     console.log("current = " + current);
-    document.getElementById("current--0").textContent = current;
+    document.getElementById(`current--${playerNumber}`).textContent = current;
     return current;
 }
 
@@ -34,7 +40,7 @@ content.classList.add('playerwins' ,'');
 container1.appendChild(content);
 
 
-const switchPlayer = function () {
+  function  switchPlayer() {
   const player1 = document.getElementsByClassName('player player--0')[0];
   const player2 = document.getElementsByClassName('player player--1')[0];
 
@@ -48,14 +54,38 @@ const switchPlayer = function () {
 };
 
 
-function hold(){
-    let current0 = document.querySelector('current--0');
-    let current1 = document.querySelector('current--1');
-    let score0 = document.querySelector('score--0');
-    let score1 = document.querySelector('score--1');
+function hold() {
+    let current0 = parseFloat(document.querySelector('#current--0').textContent);
+    let current1 = parseFloat(document.querySelector('#current--1').textContent);
+    let score0 = parseFloat(document.querySelector('#score--0').textContent);
+    let score1 = parseFloat(document.querySelector('#score--1').textContent);
+   
     document.getElementById('score--0').textContent = score0 + current0;
-    console.log(score0)
     document.getElementById('score--1').textContent = score1 + current1;
-    switchPlayer()
+   
+    switchPlayer();
+  }
+
+function reset()
+{
+    document.getElementById('current--0').textContent=0;
+    document.getElementById('current--1').textContent=0;
+    document.getElementById('score--0').textContent=0;
+    document.getElementById('score--0').textContent=0;
+    const diceImage = document.querySelector(".dice");
+    const imageName = ``;
+    diceImage.src = imageName;
 }
 
+function getActivePlayer() {
+    const player0 = document.getElementsByClassName('player player--0')[0];
+    const player1 = document.getElementsByClassName('player player--1')[0];
+ 
+    if (player0.classList.contains('player--active')) {
+        return 0;
+    } else if (player1.classList.contains('player--active')) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
